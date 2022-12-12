cdccdb1f597544b36ef37c1a91fac7d2fc37e941
Daenarys <androidwoz@gmail.com>
2022-12-12 01:12:31 +0100

    Revert "more stable for now"
    This reverts commit b1f85f3e0a8081b272aaa8203f337dd3553dc610.

--- RazerNaga/actionButton.lua
+++ RazerNaga/actionButton.lua
@@ -138,21 +138,21 @@ local SecureHandler = RazerNaga:CreateHiddenFrame('Frame', nil, nil, "SecureHand
     -- use some behaviors only available to blizzard action buttons, mainly cast on
     -- key down and press and hold casting
 local function proxyActionButton(owner, target)
+	if not target then return end
     -- disable paging on the target by giving the target an ID of zero
      target:SetID(0)
 
     -- display the target's binding action
     owner.commandName = target.commandName
 
-    -- ensure the target's action matches the parent's action
-	local proxy = CreateFrame('Frame', nil, nil, "SecureHandlerBaseTemplate")
-    proxy:Hide()
-    proxy:SetFrameRef("target", target)
-    proxy:WrapScript(owner, "OnAttributeChanged", [[
-    if name ~= "action" then return end
-        local target = control:GetFrameRef("target")
+   -- mirror the owner's action on target whenever it changes
+    SecureHandlerSetFrameRef(owner, "ProxyTarget", target)
+
+    SecureHandler:WrapScript(owner, "OnAttributeChanged", [[
+        if name ~= "action" then return end
+        local target = self:GetFrameRef("ProxyTarget")
         if target and target:GetAttribute(name) ~= value then
-           target:SetAttribute(name, value)
+			target:SetAttribute(name, value)
         end
     ]])
 
@@ -165,8 +165,6 @@ end
 local function createActionButton(id)
     local buttonName = ('%sActionButton%d'):format('RazerNaga', id)
     local button = CreateFrame('CheckButton', buttonName, nil, 'ActionBarButtonTemplate')
-	
-	local target = RazerNaga.BlizzardActionButtons[id]
 
     -- inject custom flyout handling
     RazerNaga.SpellFlyout:WrapScript(button, "OnClick", [[
@@ -180,9 +178,7 @@ local function createActionButton(id)
         end
     ]])
 
-    if target then
-       proxyActionButton(button, target)
-    end
+    proxyActionButton(button, RazerNaga.BlizzardActionButtons[id])
 
     return button
 end
